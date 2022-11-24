0f477912302008ad845347a94d3c9505c46fe709
Michael Herrera <Michael Herrera>
2022-11-23 23:01:10 -0500

    capturando-valores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba-tecnica';
+  routeList: Object;
+  origen: any;
+  destino: any;
+
+  
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(){
+    this.dataApi()
+  }
+
+  dataApi(){
+    this.apiService.getRoute().subscribe((data)=>{
+      this.routeList = data;
+      console.log(this.routeList);
+    });
+  }
+
+  capturarOrigen(e){
+    this.origen = e.srcElement.value
+    console.log("origen seleccionado" , this.origen)
+  }
+
+  capturarDestino(e){
+    this.destino = e.srcElement.value
+    console.log("destino seleccionado" , this.destino)
+  }
 }

71b016ac61df5425ed06fb80ff4dd6062c5a3948
Michael Herrera <Michael Herrera>
2022-11-24 22:35:04 -0500

    buscar-ruta

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
+  length: number;
   
   constructor(private httpClient: HttpClient) { }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { ApiService } from './api.service';
+import { FormBuilder, FormControl, FormGroup, Validators, } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,22 @@ import { ApiService } from './api.service';
 })
 export class AppComponent {
   title = 'prueba-tecnica';
-  routeList: Object;
+  routeList: any;
   origen: any;
   destino: any;
+  checkoutForm: FormGroup;
+  submitted: boolean;
+  rutaOrigen: any;
 
   
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit(){
     this.dataApi()
+    this.checkoutForm = this.formBuilder.group({
+      origen: ['', Validators.required],
+      destino: ['', Validators.required],
+    });
   }
 
   dataApi(){
@@ -35,4 +44,22 @@ export class AppComponent {
     this.destino = e.srcElement.value
     console.log("destino seleccionado" , this.destino)
   }
+
+  onSubmit(){
+    this.submitted = true
+  }
+
+  bucarVuelo(){
+    let ruta = [];
+    for (let i = 0; i < this.routeList.length; i++) {
+        // if (this.routeList[i].departureStation == this.origen ) {
+        //     bigCities.push(this.routeList[i]);
+        // }
+        if(this.routeList[i].departureStation == this.origen && this.routeList[i].arrivalStation == this.destino){
+          ruta.push(this.routeList[i]);
+        }
+    }
+    console.log("ruta deseada", ruta);
+  }
+ 
 }
